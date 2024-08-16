1c1fe815bbd417a408fcdc50122f79acecdfcc3a
Kevin Peery <kevin.peery@gmail.com>
2024-08-15 21:35:21 -0500

    complete

---
+++ src/components/HackerNews/HackerNews.jsx
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { requestArticles } from "../../reducers/hackerNewsReducer.js";
+import { useEffect } from "react";
+import Card from "../shared/Card/Card.jsx";
+import Loading from "../shared/Loading/Loading.jsx";
+
+export default function HackerNews() {
+  const articles = useSelector((state) => state.hackerNews.articles);
+  const loading = useSelector((state) => state.hackerNews.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(requestArticles);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const articleCards = articles.map((article) => (
+    <Card key={article.id} article={article} />
+  ));
+  return (
+    <div className="news-container">
+      <img className="logo" src="../../assets/hackerNews.jpeg" alt="" />
+      {loading ? <Loading /> : <div>{articleCards}</div>}
+    </div>
+  );
+}
